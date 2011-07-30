257dc09c138cc648b46d4276ff2e32d49dc7bc58
Ankur Oberoi <aoberoi@gmail.com>
2011-07-29 22:57:11 -0700

    dynamic loading logic

--- js/jobfair.js
+++ js/jobfair.js
@@ -1,7 +1,75 @@
-function onLinkedInLoad() {
-     IN.Event.on(IN, "auth", onLinkedInAuth);
-}
+var JOBFAIR = (function (IN) { 
+  var jf = {}; 
+  //privateVariable = 1; 
+     
+  //function privateMethod() { 
+  // ... 
+  //} 
+     
+  //jf.moduleProperty = 1;
+	
+	var userStates = {
+		LOBBY : 0,
+		CHATTING : 1,
+		IN_QUEUE : 2
+	};
+	
+	var userTypes = {
+		JOB_SEEKER : 0,
+		COMPANY_REP : 1
+	};
+	
+	var me = {
+		type : null,
+		linkedInId : null,
+		state: null
+	}
 
-function onLinkedInAuth() {
-	console.log("authorized");
-}
+  /*
+   *  Layout Initializations
+   */
+   
+  function initUserChoice() {
+ 	  $('.lobby_button').click(function() {
+ 	    console.log('user choice made');
+ 	    // TODO: verify its one of known user types
+ 	    me.type = $(this).data('userType');
+ 	    // TODO: use dispatched events and event listeners
+ 	    jf.onUserSelected();
+ 	  })
+ 	}
+ 	
+ 	function initTableList() {
+ 	  // attach all event handlers
+ 	  
+ 	}
+ 	
+ 	/*
+ 	 * Helpers
+ 	 */
+	
+	function loadLayout(layoutName, callback) {		
+		$('#shell').load('layout/'+layoutName+'.php', function() {
+		  console.log(layoutName + ' layout loaded');
+		  callback();
+		});
+	}
+	
+	/*
+ 	 * Handlers (Public)
+ 	 */
+	
+  jf.onLinkedInLoad = function() { 
+      IN.Event.on(IN, "auth", jf.onLinkedInAuth);
+  };
+
+	jf.onLinkedInAuth = function() {
+		loadLayout('user_choice', initUserChoice);
+	};
+	
+	jf.onUserSelected = function() {
+	  loadLayout('table_list', initTableList);
+	}
+     
+  return jf; 
+}(IN));
---
+++ layout/user_choice.php
@@ -0,0 +1,4 @@
+<div id="user_choice_container">
+	<a id="seeker_button" class="lobby_button" data-user-type="0" onclick="console.log('test');">Job Seeker</a>
+	<a id="rep_button" class="lobby_button" data-user-type="1" onclick="console.log('test');">Representative</a>
+</div>
