2d590afac3b1f3696bd8f2c26c1945b9b7b7c0ec
Ezra Velazquez <ezra.ev@gmail.com>
2011-07-30 01:41:53 -0700

    css updates

---
+++ chat.php
@@ -0,0 +1,26 @@
+<?php
+?>
+<div id="list_container">
+		<div id="top_container">
+			<div id="top_left_container">
+				<div id="top_left_container_label">Video Feed</div>
+				<div id="other_video_container"></div>
+				<div id="my_video_container"></div>
+				<div id="next_person_button">Next Person</div>
+			</div>
+			<div id="resume_container">
+				<div id="resume_label">LinkedIn User Information</div>
+				<div id="resume_info">
+					<div id="profile"></div>
+				</div>
+			</div>
+		</div>
+		<div id="bottom_container">
+			<div id="bottom_container_label">Instant Sharing</div>
+			<div id="instant_chat_container">
+				<div id="instant_chat_public">blah blah blah</div>
+				<input type="text" id="instant_chat_entry" placeholder="enter something to share"/>
+				<div id="instant_chat_button">Press to Share</div>
+			</div>
+		</div>
+	</div>
