4832ea85bf0e4bf70a800ff928a5c3936f966907
Ezra Velazquez <ezra.ev@gmail.com>
2011-07-29 22:57:50 -0700

    css updates

---
+++ table_list.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<div id="table_list_container">
+		<div class="company_profile">
+			<div class="company_profile_name">TokBox</div>
+			<div class="company_profile_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+			<div class="company_profile_positions_label">Seeking:</div>
+			<div class="company_profile_positions">Tech Support, HR Recruit, Dev Envangelist</div>
+			<div class="company_queue_container">
+				<div class="company_queue_normal_state">
+					In Queue
+					<div class="company_queue_number">12</div>
+				</div>
+				<div class="company_queue_hover_state">Talk to Rep</div>
+			</div>
+		</div>
+</div>
